b6b026c15301010e1189b4784e4d3e5c550c1c74
Sabău Florin <36925871+Rorgon@users.noreply.github.com>
2019-07-06 15:27:10 +0300

    Revert "Update random.hpp"
    This reverts commit 6a217a2f33f81c30034792ad7ba4f780d6ce3e48, reversing
    changes made to 6fca6c8dc584914dc053c6b2060593ca19b3bb0c.

--- random.hpp
+++ random.hpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <random>
 #include <chrono>
 #include <vector>
@@ -57,41 +56,3 @@ private:
 
 
 
-=======
-#include <random>
-#include <chrono>
-
-int randomInt(int m,int M)
-{
-    unsigned int seed= std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine generator(seed);
-    std::uniform_int_distribution<int> distribution(m,M);
-    return distribution(generator);
-}
-
-long long randomLongLong(long long m,long long M)
-{
-    unsigned int seed= std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine generator(seed);
-    std::uniform_int_distribution<long long> distribution(m,M);
-    return distribution(generator);
-}
-
-float randomFloat(float m,float M)
-{
-    unsigned int seed= std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine generator(seed);
-    std::uniform_real_distribution<float> distribution(m,M);
-    return distribution(generator);
-}
-
-double randomDouble(double m,double M)
-{
-    unsigned int seed= std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine generator(seed);
-    std::uniform_real_distribution<double> distribution(m,M);
-    return distribution(generator);
-}
-
-
->>>>>>> 0852c8731a471954ee8fa4f99ddcb519badb8c97
